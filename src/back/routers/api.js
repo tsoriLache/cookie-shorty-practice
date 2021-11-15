12d209a7d3abd246f989738a8f6397d4229371c4
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-15 13:17:57 +0200

    prettier

--- src/back/routers/api.js
+++ src/back/routers/api.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const urlValidator = require('./urlValidator')
-const {getCounter,searchUrlInDB,addNewUrlToDB, getOriginalUrl, getCreationDate} = require('../db/db.js')
+const urlValidator = require('./urlValidator');
+const {
+  getCounter,
+  searchUrlInDB,
+  addNewUrlToDB,
+  getOriginalUrl,
+  getCreationDate,
+} = require('../db/db.js');
 
-router.post('/shorturl/',urlValidator, function (req, res) {
-    const {originalUrl} = req.body;
-    if(searchUrlInDB(originalUrl)){
-        res.end(`${searchUrlInDB(originalUrl)}`)
-    }else{
-        try{
-            const uid = addNewUrlToDB(originalUrl)
-            res.end(`/s/${uid}`);
-        }catch(err){
-            next({status:500,message:'problem in DB'})
-        }
+router.post('/shorturl/', urlValidator, function (req, res) {
+  const { originalUrl } = req.body;
+  if (searchUrlInDB(originalUrl)) {
+    res.end(`${searchUrlInDB(originalUrl)}`);
+  } else {
+    try {
+      const uid = addNewUrlToDB(originalUrl);
+      res.end(`/s/${uid}`);
+    } catch (err) {
+      next({ status: 500, message: 'problem in DB' });
     }
-})
+  }
+});
 
 router.get('/statistic/:uid', function (req, res) {
-    const { uid } = req.params;
-    const stats = { originalUrl:getOriginalUrl(uid),
-                    shortUrl:`/s/${uid}`,
-                    dateCrated:getCreationDate(uid),
-                    counter:getCounter(uid)
-                  }
-    res.json(stats)
-})
+  const { uid } = req.params;
+  const stats = {
+    originalUrl: getOriginalUrl(uid),
+    shortUrl: `/s/${uid}`,
+    dateCrated: getCreationDate(uid),
+    counter: getCounter(uid),
+  };
+  res.json(stats);
+});
 
 module.exports = router;
