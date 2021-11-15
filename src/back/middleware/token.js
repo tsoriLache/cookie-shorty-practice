9a1619b0894e832c820251a4f9db1053ec566035
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-15 16:58:26 +0200

    changed getting token from cookies insted of header

--- src/back/middleware/token.js
+++ src/back/middleware/token.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
-  const token = req.headers['auth'];
+  const { token } = req.cookies;
   if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, user) {
