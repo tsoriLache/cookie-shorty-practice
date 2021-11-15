65a3f7064adbed076a854b50fa85704ab5484478
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-15 13:45:33 +0200

    register works with mongodb
    needs to add incription

--- src/back/routers/register.js
+++ src/back/routers/register.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 const User = require('../mongodb/models/user');
 
-router.post('/', function (req, res) {
+router.post('/', async function (req, res) {
   const { username, password } = req.body;
-  //check if alreay register
-  //incript password
-  //save to db
-  User.insertMany([{ username, password }]);
-  res.send('reg');
+  //check if already register
+  const result = await User.find({ username });
+  if (result[0]) {
+    res.send('already registered');
+  } else {
+    //incript password
+
+    //save to db
+    User.insertMany([{ username, password }]);
+    res.send('register succeeded');
+  }
 });
 
 module.exports = router;
