e8a7bb2b71a19a7ec8509f52c711594f6e103163
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-15 16:15:26 +0200

    authenticate token

---
+++ src/back/middleware/token.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+  const token = req.headers['auth'];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, user) {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = authenticateToken;
